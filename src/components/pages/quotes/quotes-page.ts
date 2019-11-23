2cdd37b98917d87cffa70222a7c5f475191f7aed
william <william@adamantio.com.br>
2019-11-23 14:12:29 -0300

    configurado o angular

---
+++ src/components/pages/quotes/quotes-page.ts
@@ -0,0 +1,28 @@
+
+/**
+  * @ngdoc component
+  * @name quotes.component:quotesPage
+  * @description
+  * Componente referente à tela de listagem de cotações
+  * 
+*/
+angular.module('quotes').component('quotesPage',{
+    templateUrl:'./src/components/pages/quotes/quotes-page.html',
+    controller:quotesPageController,
+    bindings:{
+        
+    }
+})
+
+
+/**
+  * @this vm
+  * @ngdoc controller
+  * @name quotes.controller:quotesPageController
+  *
+  * @description
+  * Controlador do componente quotesPage
+*/
+function quotesPageController(){
+
+}
--- src/module/quotes.js
+++ src/module/quotes.js
@@ -1,1 +1,32 @@
-let quotesApp = angular.module("quotes",[]);
+
+
+/**
+  * @ngdoc overview
+  * @name quotes.module:quotes
+  *
+  * @description
+  * Módulo responsável pela exibição das quotações de ações
+  * 
+  * @example
+  *  <b>script.js</b>
+  *  <pre>
+  *  angular.module('quotes', [quotes]);
+  *  </pre>
+  * 
+*/
+let quotesApp = angular.module("quotes",['ui.router']);
+
+
+quotesApp.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('home', {
+      url: '/',
+      views:{
+          content:{
+              template:'<quotes-page></quotes-page>'
+          }
+      }
+    });
+}]);
